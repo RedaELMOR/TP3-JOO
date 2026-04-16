0937a11eb9bac3b25e394b76ae3c84aa64ac6cce
Mohamed Reda EL MORABIT <relmorabit8@gmail.com>
2026-04-16 01:34:32 +0100

    EX7.java

--- EX7.java
+++ EX7.java
@@ -1,41 +1,78 @@
 package packkTP3;
 
-class Produit {
-    private String nom;
-    private double prix;
+import java.util.ArrayList;
 
-    public Produit(String nom, double prix) {
+class Produit {
+
+    private int id;
+    private String nom;
+    private String description;
+    private double prix;
+    private int quantite;
+
+    public Produit(int id, String nom, String description, double prix, int quantite) {
+        this.id = id;
         this.nom = nom;
+        this.description = description;
         this.prix = prix;
+        this.quantite = quantite;
     }
 
-    public void appliquerRemise(double pourcentage) {
-        if (pourcentage > 0 && pourcentage <= 100) {
-            prix = prix - (prix * pourcentage / 100);
+    public static void ajouterProd(ArrayList<Produit> liste, Produit p) {
+        liste.add(p);
+    }
+
+    public void modifierProd(String nom, String description, double prix, int quantite) {
+        this.nom = nom;
+        this.description = description;
+        this.prix = prix;
+        this.quantite = quantite;
+    }
+
+    public static void supprimerProd(ArrayList<Produit> liste, int id) {
+        for (Produit p : liste) {
+            if (p.id == id) {
+                liste.remove(p);
+                break;
+            }
         }
     }
 
+    public static Produit getProduitByID(ArrayList<Produit> liste, int id) {
+        for (Produit p : liste) {
+            if (p.id == id) {
+                return p;
+            }
+        }
+        return null;
+    }
+
     public void afficher() {
-        System.out.println("Produit : " + nom + "  Prix : " + prix);
+        System.out.println("ID: " + id +
+                ", Nom: " + nom +
+                ", Desc: " + description +
+                ", Prix: " + prix +
+                ", Quantité: " + quantite);
     }
 }
-
 public class EX7 {
     public static void main(String[] args) {
 
-        Produit p1 = new Produit("PC", 10000);
-        Produit p2 = new Produit("Telephone", 5000);
+        ArrayList<Produit> liste = new ArrayList<>();
 
-        System.out.println("Produits initiaux :");
-        p1.afficher();
-        p2.afficher();
+        Produit p1 = new Produit(1, "PC", "Gaming", 8000, 5);
+        Produit p2 = new Produit(2, "Souris", "RGB", 200, 10);
 
-        p1.appliquerRemise(10);
-        System.out.println("Apres remise sur PC :");
-        p1.afficher();
+        Produit.ajouterProd(liste, p1);
+        Produit.ajouterProd(liste, p2);
 
-        p2.appliquerRemise(20);
-        System.out.println("Apres remise sur Telephone :");
-        p2.afficher();
+        for (Produit p : liste) {
+            p.afficher();
+        }
+        p1.modifierProd("PC", "Gaming RTX", 9000, 3);
+        Produit p = Produit.getProduitByID(liste, 1);
+        if (p != null) p.afficher();
+
+        Produit.supprimerProd(liste, 2);
     }
 }
